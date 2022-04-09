009c5d1eb85da8596a1a8dab94adf22fcf5599aa
Alexandre010xC <100102240+Alexandre010xC@users.noreply.github.com>
2022-04-08 22:03:33 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+
+void cmd_input(char cmd[], char *params[]){
+  char line[1024],*array[100],*pch;
+  int count=0, i=0;
+
+  for(;;){
+    int c = fgetc(stdin);
+    line[count++] = (char) c;
+    if(c=='\n')
+      break;
+  }
+
+  if(count==1)
+    return;
+  
+  pch = strtok(line," \n");
+
+  while(pch!=NULL){
+    array[i++]=strdup(pch);
+    pch=strtok(NULL," \n");
+  }
+
+  strcpy(cmd,array[0]);
+
+  for(int j=0;j<i;j++)
+    params[j]=array[j];
+  params[i]=NULL;
+}
+
+int main(void){
+
+  char cmd[100], command[100], *parameters[20], *env[]={(char*) "PATH=/bin",0};
+  int ch;
+  
+  system("clear");
+  while(1){
+    printf("myshell> ");
+
+    cmd_input(command,parameters);
+    // scanf("%[^\n]s",command);
+
+    while ((ch = getchar()) != '\n');
+
+    if(strcmp(command,"exit")==0)
+      break;
+
+    if(fork()!=0){
+      wait(NULL);
+    }else{
+      strcpy(cmd,"/bin/");
+      strcat(cmd,command);
+      execve(cmd,parameters,env);
+    }
+  }
+
+  return 0;
+}
